981f741072fb4dd423c93b2f015e5c056a5de2f5
gusm-77 <shubhamondal811@gmail.com>
2026-06-21 12:28:22 +0530

    Add after shorting count

--- arr1.cpp
+++ arr1.cpp
@@ -7,11 +7,13 @@ int arr[]={10,12,11,15,14,13,17,16};
 int main(){
     int *ptr;
     ptr=find(arr,arr+8,15);
-    cout<<"Find the vale after sorting :"<<(ptr-arr)<<endl;
+    cout<<"Find the value before sorting :"<<(ptr-arr)<<endl;
     sort(arr,arr+8);
     for(int j=0;j<8;j++){
         cout<<arr[j]<<" ";
     }
     cout<<endl;
+    int *ptr1=find(arr,arr+8,15);
+    cout<<"Find the value after sorting : "<<(ptr1-arr)<<endl;
     return 0;
 }
