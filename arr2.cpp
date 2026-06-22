319f85a7a3c1296d1b05de866d8a0ac27f5ed101
gusm-77 <shubhamondal811@gmail.com>
2026-06-22 12:42:49 +0530

    Merge array

---
+++ arr2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int arr1[]={10,16,13,14,8,9,13};
+int arr2[]={21,23,25,96,27};
+
+int dest[12];
+
+int main(){
+    merge(arr1,arr1+7,arr2,arr2+5,dest);
+    for( int j=0;j<12;j++){
+        cout<<dest[j]<<" ";
+    }
+    cout<<endl;
+    return 0;
+
+}
